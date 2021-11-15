c3cd9abccfd634f8f3e6181dd59a33c48ae8bad8
soumyabhardwaj <63638319+soumyabhardwaj@users.noreply.github.com>
2021-11-16 02:12:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,10 +51,7 @@ function App() {
   const { user, initializing } = useAuthState(firebase.auth());
   const [darkMode, setDarkMode] = useDarkMode();
 
-  const brandLogo = darkMode
-    ? `${process.env.PUBLIC_URL}/logo_white.svg`
-    : `${process.env.PUBLIC_URL}/logo.svg`;
-
+  
   const ThemeIcon = darkMode ? SunIcon : MoonIcon;
 
   const signInWithGoogle = async () => {
